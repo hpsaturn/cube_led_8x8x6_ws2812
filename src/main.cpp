a6599ba7a71a1d2a96569c3e75ddce14a7445c41
Antonio Vanegas <hpsaturn@gmail.com>
2020-04-10 15:18:51 +0200

    test ready for controlling each panel

--- src/main.cpp
+++ src/main.cpp
@@ -1,275 +1,39 @@
 #include <FastLED.h>
-#include <BluetoothSerial.h>
+#include <FastLED_GFX.h>
 
-#define DEEP_SLEEP_DURATION 30 // sleep x seconds and then wake up
+#define LED_PIN         16
+#define COLOR_ORDER     GRB
+#define CHIPSET         WS2812
+#define BRIGHTNESS      64
 
-#define LED_PIN     16
-#define NUM_LEDS    8*8*6
-#define LED_TYPE    WS2812
-#define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS];
+#define CANVAS_WIDTH    8
+#define CANVAS_HEIGHT   48
+#define NUM_LEDS        (CANVAS_WIDTH * CANVAS_HEIGHT)
 
-#define UPDATES_PER_SECOND 100
-
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
-
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
-
-BluetoothSerial btSerial; //Object for Bluetooth
-int incoming;
-bool enablePaletteDemo=true;
-uint8_t speed = 1;
-uint8_t mbrightness = 30;
-
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-    // 'black out' all 16 palette entries...
-    fill_solid( currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
-    
-}
-
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
-}
-
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
-{
-    
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, mbrightness, currentBlending);
-        colorIndex += 3;
-    }
-}
-
-
-// There are several different palettes of colors demonstrated here.
-//
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.  All are shown here.
-
-void ChangePalettePeriodically()
-{
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-    
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
-
-        if(secondHand==10){
-            btSerial.print(".");
-        }     
-    }
-}
-
-// This example shows how to set up a static color palette
-// which is stored in PROGMEM (flash), which is almost always more
-// plentiful than RAM.  A static PROGMEM palette like this
-// takes up 64 bytes of flash.
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
-};
-
-// Additional notes on FastLED compact palettes:
-//
-// Normally, in computer graphics, the palette (or "color lookup table")
-// has 256 entries, each containing a specific 24-bit RGB color.  You can then
-// index into the color palette using a simple 8-bit (one byte) value.
-// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
-// is quite possibly "too many" bytes.
-//
-// FastLED does offer traditional 256-element palettes, for setups that
-// can afford the 768-byte cost in RAM.
-//
-// However, FastLED also offers a compact alternative.  FastLED offers
-// palettes that store 16 distinct entries, but can be accessed AS IF
-// they actually have 256 entries; this is accomplished by interpolating
-// between the 16 explicit entries to create fifteen intermediate palette
-// entries between each pair.
-//
-// So for example, if you set the first two explicit entries of a compact 
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
-// the first sixteen entries from the virtual palette (of 256), you'd get
-// Green, followed by a smooth gradient from green-to-blue, and then Blue.
-
-void gotToSuspend (){
-  Serial.println("-->[ESP] suspending..");
-  delay(10); // waiting for writing msg on serial
-  //esp_sleep_enable_timer_wakeup(1000000LL * DEEP_SLEEP_DURATION);
-  esp_deep_sleep_start();
-}
-
+GFXcanvas canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 
 void setup() {
-    delay( 3000 ); // power-up safety delay
-    Serial.begin(115200);
-    btSerial.begin("CubeLED");
-    Serial.println("BT Serial ready");
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness( mbrightness );
-    
-    currentPalette = RainbowColors_p;
-    currentBlending = LINEARBLEND;
-    Serial.println("Setup ready.");
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(canvas.getBuffer(), NUM_LEDS).setCorrection(TypicalSMD5050);
+  FastLED.setBrightness(BRIGHTNESS);
+  delay(100);
+  FastLED.clear();
+  FastLED.show();
 }
 
-void animPaletteLoop() {
+uint8_t panel = 0;
 
-    ChangePalettePeriodically();
-
-    static uint8_t startIndex = 0;
-    startIndex = startIndex + speed; /* motion speed */
-
-    FillLEDsFromPaletteColors(startIndex);
-
+void loop() {
+  EVERY_N_SECONDS(1) {
+    // canvas.fillScreen(CRGB::Red);
+    canvas.fillScreen(CRGB::Blue);
+    canvas.drawRect(0,panel,8,8,CRGB::Red);
+    canvas.drawRect(0,32,8,8,CRGB::Yellow);
+    canvas.drawRect(0,40,8,8,CRGB::Green);
+    // canvas.drawLine(0, 0, CANVAS_WIDTH-1, CANVAS_HEIGHT-1, CRGB::White);
+    // canvas.drawCircle(4, 4, 2, CRGB(255,255,0));
+    panel=panel+8;
+    if(panel==32)panel=0;
     FastLED.show();
-    FastLED.delay(1000 / UPDATES_PER_SECOND);
+  }
 }
 
-void loop()
-{
-    if (btSerial.isReady())
-    {
-        incoming = btSerial.read(); //Read what we recevive
-
-        if (incoming == 49) {
-            digitalWrite(LED_BUILTIN, HIGH);
-            enablePaletteDemo=true;
-            Serial.println("ON");
-            btSerial.println("Animation turned ON");
-        }
-
-        if (incoming == 48) {
-            digitalWrite(LED_BUILTIN, LOW);
-            enablePaletteDemo=false;
-            FastLED.clear();
-            FastLED.show();
-            Serial.println("OFF");
-            btSerial.println("Animation turned OFF");
-        }
-
-        if (incoming == 43) {
-            btSerial.println("Speed Up");
-            speed++;
-            btSerial.print("speed: ");
-            btSerial.println(speed);
-            Serial.println("Speed Up");
-        }
-
-        if (incoming == 45) {
-            btSerial.println("Speed Down");
-            if(speed>0) speed--;
-            btSerial.print("speed: ");
-            btSerial.println(speed);
-            Serial.println("Speed Down");
-        }
-
-        if (incoming == 80) {
-            btSerial.println("Shutdown ESP32..");
-            Serial.println("Shutdown ESP32..");
-            FastLED.clear();
-            FastLED.show();
-            delay(1000);
-            btSerial.println("halt.");
-            gotToSuspend();
-        }
-
-        if (incoming == 53) {
-            btSerial.println("Brightness Up");
-            if(mbrightness<254)mbrightness++;
-            btSerial.print("brightness: ");
-            btSerial.println(mbrightness);
-            Serial.println("Brightness Up");
-        }
-
-        if (incoming == 52) {
-            btSerial.println("Brighness Down");
-            if(mbrightness>0) mbrightness--;
-            btSerial.print("brightness: ");
-            btSerial.println(mbrightness);
-            Serial.println("Brightness Down");
-        }
-    }
-
-    if(enablePaletteDemo) animPaletteLoop();
-
-}
